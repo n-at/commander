25b53d70064ab1dbcef2b8f415506f44cac6f69f
Alexey Nurgaliev <atnurgaliev@gmail.com>
2014-11-03 11:30:09 +0300

    preset scripts execution

--- conf/index.js
+++ conf/index.js
@@ -10,6 +10,7 @@ var root = path.join(__dirname, '..');
 nconf.set('root_path', root);
 nconf.set('log_path', path.join(root, 'log'));
 nconf.set('tmp_path', path.join(root, 'tmp'));
+nconf.set('preset_path', path.join(root, 'preset'));
 
 //create directories
 mkdirp.sync(nconf.get('log_path'));
---
+++ test/test_preset_script.js
@@ -0,0 +1,31 @@
+var runner = require('../lib/runner');
+
+//running existing script
+runner.executePreset('uname.sh', function(err, result) {
+    console.log('Existing preset script');
+    if(err) {
+        console.log('Error: %s', err.message);
+    } else {
+        console.log(result);
+    }
+});
+
+//running not existing script
+runner.executePreset('error', function(err, result) {
+    console.log('Not existing preset script');
+    if(err) {
+        console.log('Error: %s', err.message);
+    } else {
+        console.log(result);
+    }
+});
+
+//test security violation
+runner.executePreset('../unit.js', function(err, result) {
+    console.log('Security violation test');
+    if(err) {
+        console.log('Error: %s', err.message);
+    } else {
+        console.log(result);
+    }
+});

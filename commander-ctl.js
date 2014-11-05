6abf47f104ebac1a0b4e556ac2ff94e6d97473e2
Alexey Nurgaliev <atnurgaliev@gmail.com>
2014-11-05 17:24:25 +0300

    separate daemon module closes #4

--- commander-ctl.js
+++ commander-ctl.js
@@ -1,31 +1,7 @@
-var daemon = require('daemonize2').setup({
-    main: 'commander.js',
-    pidfile: 'commander.pid',
+var daemonOptions = {
+    main: '../commander.js',
+    pidfile: '../commander.pid',
     name: 'commander server'
-});
-switch(process.argv[2]) {
-    case 'start':
-        daemon.start();
-        break;
-    case 'stop':
-        daemon.stop();
-        break;
-    case 'restart':
-        daemon.stop(function() {
-            daemon.start();
-        });
-        break;
-    case 'status':
-        var pid = daemon.status();
-        if(pid) {
-            console.log('commander server is running with pid:%s', pid);
-        } else {
-            console.log('commander server is not running');
-        }
-        break;
-    case 'version':
-        console.log('commander ' + require('./package').version);
-        break;
-    default:
-        console.log('Usage: [start|stop|restart|status|version]');
-}
+};
+
+require('./lib/daemon')(daemonOptions);
--- unit-ctl.js
+++ unit-ctl.js
@@ -1,31 +1,7 @@
-var daemon = require('daemonize2').setup({
-    main: 'unit.js',
-    pidfile: 'unit.pid',
+var daemonOptions = {
+    main: '../unit.js',
+    pidfile: '../unit.pid',
     name: 'commander unit server'
-});
-switch(process.argv[2]) {
-    case 'start':
-        daemon.start();
-        break;
-    case 'stop':
-        daemon.stop();
-        break;
-    case 'restart':
-        daemon.stop(function() {
-            daemon.start();
-        });
-        break;
-    case 'status':
-        var pid = daemon.status();
-        if(pid) {
-            console.log('commander unit server is running with pid:%s', pid);
-        } else {
-            console.log('commander unit is not running');
-        }
-        break;
-    case 'version':
-        console.log('commander ' + require('./package').version);
-        break;
-    default:
-        console.log('Usage: [start|stop|restart|status|version]');
-}
+};
+
+require('./lib/daemon')(daemonOptions);
